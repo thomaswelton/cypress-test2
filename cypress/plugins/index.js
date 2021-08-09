82bec531f3108a8bdc4f2502be7787757bded309
Thomas Welton <thomaswelton@me.com>
2021-08-09 12:58:00 +0100

    Use custom babel config

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,7 +1,13 @@
 /* eslint-disable */
 const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot/plugin');
+const webpackPreprocessor = require("@cypress/webpack-preprocessor");
+
+const defaults = webpackPreprocessor.defaultOptions;
 
 module.exports = (on, config) => {
+  delete defaults.webpackOptions.module.rules[0].use[0].options.presets;
+  on("file:preprocessor", webpackPreprocessor(defaults));
+
   addMatchImageSnapshotPlugin(on, config);
 
   on('before:browser:launch', (browser = {}, launchOptions) => {
